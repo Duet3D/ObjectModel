7862d89c11233f1cf5fe0384f434949c9b24bd52
Christian Hammacher <bmasterc@gmail.com>
2024-11-06 14:33:53 +0100

    Changed spindle properties

--- src/spindles/index.ts
+++ src/spindles/index.ts
@@ -7,17 +7,23 @@ export enum SpindleState {
     reverse = "reverse"
 }
 
+export enum SpindleType {
+    enaDir = "enaDir",
+    fwdRev = "fwdRev"
+}
+
 export class Spindle extends ModelObject {
-    active: number = 0;
-    canReverse: boolean = false;
-    current: number = 0;
-    frequency: number = 0;
-    idlePwm: number = 0;
-    min: number = 60;
-    minPwm: number = 0;
-    max: number = 10000;
-    maxPwm: number = 1;
+    active: number | null = null;
+    canReverse: boolean | null = null;
+    current: number | null = null;
+    frequency: number | null = null;
+    idlePwm: number | null = null;
+    min: number | null = null;
+    minPwm: number | null = null;
+    max: number | null = null;
+    maxPwm: number | null = null;
     state: SpindleState = SpindleState.unconfigured;
+    type: SpindleType | null = null;
 }
 
 export default Spindle
