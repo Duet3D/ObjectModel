04f912e314cd57dd29580dc023cbc2ea5dc18501
Christian Hammacher <bmasterc@gmail.com>
2025-03-14 12:05:18 +0100

    Removed redundant position again

--- src/sensors/FilamentMonitors/LaserFilamentMonitor.ts
+++ src/sensors/FilamentMonitors/LaserFilamentMonitor.ts
@@ -26,7 +26,6 @@ export class LaserFilamentMonitor extends Duet3DFilamentMonitor {
 
     calibrated: LaserFilamentMonitorCalibrated | null = new LaserFilamentMonitorCalibrated();
     readonly configured: LaserFilamentMonitorConfigured = new LaserFilamentMonitorConfigured();
-    position: number = 0;
 
     override update(jsonElement: any): IModelObject | null {
         if (jsonElement === null) {
