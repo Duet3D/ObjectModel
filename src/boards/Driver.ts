02e2f0135339987c3da3228b9941e10a8ffa11c7
Christian Hammacher <chr.hammacher@gmail.com>
2026-05-23 13:42:47 +0200

    Version 3.7.0-alpha.10
    Updated drivers and LED strips to match latest RRF build
    Updated dependencies

--- src/boards/Driver.ts
+++ src/boards/Driver.ts
@@ -15,12 +15,28 @@ export class DriverClosedLoop extends ModelObject {
     readonly positionError: BoardClosedLoopPositionError = new BoardClosedLoopPositionError();
 }
 
+export enum DriverMode {
+    constantOffTime = 0,
+    randomOffTime = 1,
+    spreadCycle = 2,
+    stealthChop = 3,
+    direct = 4,
+    unknown = 5
+}
+
+export class DriverConfig extends ModelObject {
+    direction: boolean = true;
+    mode: DriverMode = DriverMode.spreadCycle;
+}
+
 export default class Driver extends ModelObject {
     constructor() {
         super();
         ModelObject.wrapModelProperty(this, "closedLoop", DriverClosedLoop);
+        ModelObject.wrapModelProperty(this, "config", DriverConfig);
     }
 
     closedLoop: DriverClosedLoop | null = null;
+    config: DriverConfig | null = null;
     status: number = 0;
 }
--- src/ledStrips/index.ts
+++ src/ledStrips/index.ts
@@ -6,8 +6,19 @@ export enum LedStripType {
     NeoPixel_RGBW = "NeoPixel_RGBW"
 }
 
+export enum LedStripColorOrder {
+    BGR = 0,
+    BRG = 1,
+    RGB = 2,
+    RBG = 3,
+    GBR = 4,
+    GRB = 5
+}
+
 export class LedStrip extends ModelObject {
     board: number = 0;
+    colorOrder: LedStripColorOrder = LedStripColorOrder.GRB;
+    maxLeds: number = 0;
     pin: string = "";
     stopMovement: boolean = false;
     type: LedStripType = LedStripType.DotStar;
