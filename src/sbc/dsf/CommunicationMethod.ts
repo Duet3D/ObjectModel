a47bd3337f6d7a127742f3c51ad9bb5e213c00e9
Christian Hammacher <chr.hammacher@gmail.com>
2026-05-26 17:52:15 +0200

    Expose DSF communication method via OM

---
+++ src/sbc/dsf/CommunicationMethod.ts
@@ -0,0 +1,6 @@
+export enum CommunicationMethod {
+    spi = "spi",
+    usb = "usb"
+}
+
+export default CommunicationMethod;
--- src/sbc/dsf/index.ts
+++ src/sbc/dsf/index.ts
@@ -1,19 +1,22 @@
 import ModelCollection from "../../ModelCollection";
 import ModelObject from "../../ModelObject";
 
+import CommunicationMethod from "./CommunicationMethod";
 import HttpEndpoint from "./HttpEndpoint";
 import UserSession from "./UserSession";
 
 export class DSF extends ModelObject {
     buildDateTime: string = "";
+    communicationMethod: CommunicationMethod = CommunicationMethod.spi;
     readonly httpEndpoints: ModelCollection<HttpEndpoint> = new ModelCollection(HttpEndpoint);
     is64Bit: boolean = false;
     pluginSupport: boolean = false;
     rootPluginSupport: boolean = false;
-	readonly userSessions: ModelCollection<UserSession> = new ModelCollection(UserSession);
+    readonly userSessions: ModelCollection<UserSession> = new ModelCollection(UserSession);
     version: string = "";
 }
 
 export default DSF;
 
+export * from "./CommunicationMethod";
 export * from "./HttpEndpoint";
