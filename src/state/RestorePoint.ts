25d7e9ec3810e7dda3772d119acbaa255c4e07e1
Christian Hammacher <chr.hammacher@gmail.com>
2026-07-01 21:54:30 +0200

    Version 3.7.0-beta.1
    Added G-code command number to restore points

--- src/state/RestorePoint.ts
+++ src/state/RestorePoint.ts
@@ -5,6 +5,7 @@ export class RestorePoint extends ModelObject {
     extruderPos: number = 0;
     fanPwm: number = 0;
     feedRate: number = 0;
+    gCommandNumber: number = 0;
     ioBits: number | null = null;
     laserPwm: number | null = null;
     toolNumber: number = -1;
