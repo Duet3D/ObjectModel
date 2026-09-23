9d452b088799fb6f88369606a2f7328ba623b2db
Christian Hammacher <chr.hammacher@gmail.com>
2026-09-23 15:23:13 +0200

    Version 3.7.0-rc.5
    Replaced sensors.accelerometers[].board with the port name the accelerometer was configured with

--- src/sensors/Accelerometer.ts
+++ src/sensors/Accelerometer.ts
@@ -1,9 +1,9 @@
 import ModelObject from "../ModelObject";
 
 export class Accelerometer extends ModelObject {
-    board: number = 0;
     orientation: number = 20;
     points: number = 0;
+    port: string = "";
     resolution: number = 0;
     runs: number = 0;
     samplingRate: number = 0;
